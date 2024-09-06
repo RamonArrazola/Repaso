f1db26f9a4be7b55c9c077fd502e196b8efc4942
RamonArrazola <jarrazola@exsoinf.com>
2024-09-06 17:40:34 -0600

    menu generado

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent implements OnInit {
+
+  rutas = [
+    {
+      name: 'Home',
+      path: '/home'
+    },
+    {
+      name: 'About',
+      path: '/about'
+    },
+    {
+      name: 'Contact',
+      path: '/contact'
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
