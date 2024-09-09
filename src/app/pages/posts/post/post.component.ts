020bf4372ec50fcd69c80a858213b34e95159e0a
RamonArrazola <jarrazola@exsoinf.com>
2024-09-09 15:15:19 -0600

    eventos hijo a padre terminados

--- src/app/pages/posts/post/post.component.ts
+++ src/app/pages/posts/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -8,9 +8,13 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PostComponent {
 
   @Input() post: any;
+  @Output() clickPost = new EventEmitter<number>();
 
   constructor() { }
 
   ngOnInit(){ }
 
+  Onclick(){
+    this.clickPost.emit(this.post.id);
+  }
 }
--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -14,4 +14,8 @@ export class PostsComponent implements OnInit {
   ngOnInit(){
     this.posts = this.dataService.getPosts();
   }
+
+  clickLitsen( event: number ){
+    console.log("click en " + event)
+  }
 }
