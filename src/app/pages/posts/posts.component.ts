98adcb2f78bf5475b1ea620a04c78d2ca84d263e
RamonArrazola <jarrazola@exsoinf.com>
2024-09-09 13:46:57 -0600

    Servicio de placeholder de posts generado

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.css'
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+  post: any;
+  
+  constructor(private dataService: DataService) {}
 
+  ngOnInit(){
+    this.post = this.dataService.getPosts();
+  }
 }
